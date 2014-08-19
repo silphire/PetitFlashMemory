8ba6e2543cdd970a196add7890c1bd76cba5aa8c
Yuki Mitsui <silphire@gmail.com>
2014-08-19 21:32:14 +0900

    more strict validation

--- src/info/silphire/petitflashmemory/QuestionActivity.java
+++ src/info/silphire/petitflashmemory/QuestionActivity.java
@@ -1,7 +1,9 @@
 package info.silphire.petitflashmemory;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 
 /**
  * 問題を出題していくアクティビティクラス
@@ -15,5 +17,21 @@ public class QuestionActivity extends Activity {
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_question);
+		
+		Intent intent = getIntent();
+		if(intent == null) {
+			Log.d(Constants.LOG_TAG, "Cannot obtain Intent object");
+			this.finish();
+			return;
+		}
+		
+		ProblemSet problemSet = (ProblemSet)intent.getSerializableExtra("problemSet");
+		if(problemSet == null) {
+			Log.d(Constants.LOG_TAG, "Cannot obtain associated ProblemSet");
+			this.finish();
+			return;
+		}
+
+		;
 	}
 }
